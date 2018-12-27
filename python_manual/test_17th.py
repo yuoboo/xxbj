3430f27c0718e3b1c5fc937e114815c30fda0bd6
bing01 <yu@foxmail.com>
2018-12-27 17:36:36 +0800

    manual

--- python_manual/test_17th.py
+++ python_manual/test_17th.py
@@ -54,7 +54,48 @@ def test_non():
     print('this is non')
 
 
+# 状态保持一般使用类存储比较规则, 但是使用def嵌套其实更简单
+class Tester(object):
+    def __init__(self, start):
+        self.state = start
 
+    def nested(self, label):
+        print(label, self.state)
+        self.state += 1
+
+
+class Testero(object):
+    def __init__(self, start):
+        self.state = start
+
+    def __call__(self, label):
+        print(label, self.state)
+        self.state += 1
+
+
+def tester(start):
+    state = start
+
+    def nested(label):
+        nonlocal state  # python3
+        print(label, state)
+        state += 1
+    return nested
+
+
+def funcer(start):
+    def nested(label):
+        print(label, nested.state)
+        nested.state += 1
+
+    nested.state = start
+    return nested
+
+
+def func(x=0, y=[]):
+    print(y)
+    y.append(x)
+    print(y)
 
 
 if __name__ == '__main__':
@@ -68,4 +109,21 @@ if __name__ == '__main__':
     # time.sleep(3)
     # print func_test(1)
 
-    test_non()
+    # test_non()
+
+    # f = funcer(1)
+    # f('f1')
+    # g = funcer(1)
+    # g('g1')
+    # g('g2')
+    # g('g3')
+    # f('f2')
+    # f('f3')
+
+    func()  # [0]
+    func(1, [1])  # [1,1]
+    func(1)  # [0, 1]
+    func(2)
+    func(3, [])
+    func(4)
+    func()
